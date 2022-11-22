65483f8ef474c882dfd2a08d03bda7cc69197c86
Davide Troise <davide@troise.net>
2022-11-22 11:11:28 +0100

    Initial Commit

---
+++ DarkModeDemo/DarkModeDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  DarkModeDemoApp.swift
+//  DarkModeDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct DarkModeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
