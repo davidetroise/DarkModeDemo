60aa87c15eca5734ca032abdd659883f568bed4e
Davide Troise <davide@troise.net>
2022-11-22 14:47:49 +0100

    Complete code

--- DarkModeDemo/ContentView.swift
+++ DarkModeDemo/ContentView.swift
@@ -9,12 +9,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .font(colorScheme == .dark ? .largeTitle : .body)
         }
         .padding()
     }
